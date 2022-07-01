9d9384ef60ff68b00b9e3adeee917dbed701e8a0
Valeriia <volikavs@gmail.com>
2022-07-01 21:58:03 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import throttle from 'lodash.throttle';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+// import axios from 'axios';
+import {fetchImages, resetPages} from './js/fatch-images'
 
 // import "simplelightbox/dist/simple-lightbox.min.css";
 // import { galleryItems } from './gallery-items';
 const STORAGE_KEY = 'search-form-state';
 
 const form = document.querySelector('.search-form');
-
+const galleryAll = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
 // form.addEventListener('input', throttle(onFormInput, 300));
 form.addEventListener('submit', onFormSubmit);
-
-let formInput = {
-    "searchQuery": '',
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionsDelay: 250,
+})
+btnLoadMore.addEventListener('click', onClickLoadMoreBtn);
+// let formInput = {
+//     "searchQuery": '',
+// }
 let searchText = '';
 // function onFormInput(evt) {
 //     formInput[evt.target.name] = evt.target.value;
@@ -23,9 +29,59 @@ let searchText = '';
 //     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInput));
 // }
 
-function onFormSubmit(evt) {
+async function onFormSubmit(evt) {
     evt.preventDefault();
+    clearCardsContainer();
+    resetPages();
     searchText = evt.currentTarget.searchQuery.value.trim();
     console.log(searchText);
+    const {hits} = await fetchImages(searchText);
+    evt.target.reset();
 
+    if (hits.length === 0 ) {
+        alert("Sorry, there are no images matching your search query. Please try again.");
+    }
+    renderCards(hits);
+    lightbox.refresh();
+
+}
+//  разметки карточки одного изображения для галереи
+
+function createCards(cards) {
+    return cards.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+    <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+  </a>
+</div>`).join('');
+    
+}
+
+function renderCards(cards) {
+    galleryAll.insertAdjacentHTML('beforeend', createCards(cards));
+}
+
+function clearCardsContainer() {
+    galleryAll.innerHTML = '';
+}
+
+async function onClickLoadMoreBtn() {
+    console.log("onClickLoadMoreBtn");
+    const {hits} = await fetchImages(searchText);
+    renderCards(hits);
+    lightbox.refresh();
+    
 }
--- src/js/fatch-images.js
+++ src/js/fatch-images.js
@@ -1,28 +1,35 @@
 // https://pixabay.com/api/?key=23046575-92d847e59fe9fc6af83501ff5&q=yellow+flowers&image_type=photo
 import axios from 'axios'
-export { fetchImages }
+export { fetchImages, resetPages }
+
 
 axios.defaults.baseURL = 'https://pixabay.com/api/'
       
-const KEY = '23046575-92d847e59fe9fc6af83501ff5'
+// const KEY = '23046575-92d847e59fe9fc6af83501ff5'
 
-async function fetchImages(query, page, perPage) {
+let page = 1;
+async function fetchImages(query) {
 
-const optionParam = {
-    params: {
+    const optionParam = new URLSearchParams(
+    {
         key: '23046575-92d847e59fe9fc6af83501ff5',
-        q: currentQuery,
+        q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        page,
+        page: page,
         per_page: 40,
 },
-}
+) 
 //  `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
 
-  const response = await axios.get(
-    , optionParam
-  )
-  return response
+    const {data} = await axios.get(
+        `?${optionParam}`
+    );
+    page += 1;
+    return data;
+}
+
+function resetPages() {
+    page = 1;
 }
